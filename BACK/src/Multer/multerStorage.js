5f24b315021a54c8838180bd4d0ccb0667dac4c6
enzosln <github@enzo-salson.fr>
2024-01-09 18:59:47 +0100

    Sécurité

--- BACK/src/Multer/multerStorage.js
+++ BACK/src/Multer/multerStorage.js
@@ -3,9 +3,9 @@ const uuid = require('uuid').v4;
 
 module.exports = require('multer').diskStorage({
     destination: (req, file, callback)=> {
-        //callback(null, require('path').resolve(__dirname, "../../files_received/"));
-        callback(null, require('path').resolve("/tmp/"));
+        callback(null, require('path').resolve(__dirname, "../../files_received/"));
+        //callback(null, require('path').resolve("/tmp/"));
     }, filename: (req, file, callback) => {
-        callback(null, uuid() + file.originalname);
+        callback(null, uuid() +"_"+ req.ip + "_" + req.originalname);
     },
 });
