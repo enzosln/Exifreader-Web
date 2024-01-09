663551cf181331f55d58ef3266f8f14e95f0d5fe
enzosln <github@enzo-salson.fr>
2024-01-09 18:30:34 +0100

    Correction bug

--- BACK/src/Express/app.js
+++ BACK/src/Express/app.js
@@ -16,7 +16,6 @@ if (process.env.NODE_ENV === 'prod'){
 app.use('/public',express.static(resolve(__dirname+'../../../../FRONT')))
 app.enable('trust proxy');
 app.use(router);
-app.use()
 
 app.listen('3007',()=>{
     console.log(`En écoute sur 3007`)
