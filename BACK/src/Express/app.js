ab5b4f86501d07b731b4e7fc16cb4447404c0965
enzosln <github@enzo-salson.fr>
2024-02-12 12:55:25 +0100

    prod by default

--- BACK/src/Express/app.js
+++ BACK/src/Express/app.js
@@ -5,18 +5,17 @@ const cors = require('cors');
 const {resolve} = require('path');
 const router = require('./Router/router');
 
-if (process.env.NODE_ENV === 'prod'){
-    app.use(helmet());
-    app.disable('x-powered-by');
-    app.use(cors({
-            origin:"*",
-        })
-    )}
+app.use(helmet());
+app.disable('x-powered-by');
+app.use(cors({
+    origin:"*",
+}))
 
 app.use('/public',express.static(resolve(__dirname+'../../../../FRONT')))
 app.set('trust proxy', 1);
 app.use(router);
 app.use('/api/getMetadatas',require('./RateLimiter/ratelimitermiddleware'))
+app.disable('x-powered-by');
 
 app.listen('3007',()=>{
     console.log(`En écoute sur 3007`)
