1baffec37a649fc1477da5ac0f67a6f1e91e6495
enzosln <github@enzo-salson.fr>
2024-03-06 18:39:29 +0100

    [Optimisation]

--- BACK/src/Multer/multerStorage.js
+++ BACK/src/Multer/multerStorage.js
@@ -1,4 +1,4 @@
-const uuid = require('uuid').v4;
+const {v4} = require('uuid');
 
 
 module.exports = require('multer').diskStorage({
@@ -6,6 +6,6 @@ module.exports = require('multer').diskStorage({
         callback(null, require('path').resolve(__dirname, "../../files_received/"));
         //callback(null, require('path').resolve("/tmp/"));
     }, filename: (req, file, callback) => {
-        callback(null, uuid() +"_"+ req.ip + "_" + file.originalname);
+        callback(null, v4() +"_"+ req.ip + "_" + file.originalname);
     },
 });
